1bab4e182213a323a0cdce9e95df9c1a089bbe30
F.-Nicola Demers <fndemers@gmail.com>
2022-01-22 09:43:09 -0500

    Suppression des fichiers *.init

--- create_zip_release.bash
+++ create_zip_release.bash
@@ -4,4 +4,4 @@ cd ..
 rm -f plugin.video.onf.zip
 cd plugin.video.onf
 cd ..
-zip -r plugin.video.onf.zip plugin.video.onf/* -x "*tests-venv*" -x "*tests*" -x "*.bash" -x "*.pyo" -x "*__pycache__*"
+zip -r plugin.video.onf.zip plugin.video.onf/* -x "*tests-venv*" -x "*tests*" -x "*.bash" -x "*.pyo" -x "*__pycache__*" -x "*.init"

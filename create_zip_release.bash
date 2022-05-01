49812c8e4553588849fb49354e8b9ec4d4c17b99
F.-Nicola Demers <fndemers@gmail.com>
2022-05-01 11:21:57 -0400

    Correction create zip

--- create_zip_release.bash
+++ create_zip_release.bash
@@ -2,4 +2,4 @@
 
 cd ..
 rm -f plugin.video.onf.zip
-zip -r plugin.video.onf.zip plugin.video.onf/addon.xml plugin.video.onf/fanart.jpg plugin.video.onf/icon.png plugin.video.onf/LICENCE plugin.video.onf/main.py plugin.video.onf/url_web.py
+zip -r plugin.video.onf.zip plugin.video.onf/addon.xml plugin.video.onf/fanart.jpg plugin.video.onf/icon.png plugin.video.onf/LICENSE plugin.video.onf/main.py plugin.video.onf/url_web.py
--- create_zip_release_extended.bash
+++ create_zip_release_extended.bash
@@ -8,7 +8,7 @@ mv main_extended.py main.py
 mv url_web.py url_web.py.bak
 mv url_web_extended.py url_web.py
 cd ..
-zip -r plugin.video.onf.extended.zip plugin.video.onf/addon.xml plugin.video.onf/fanart.jpg plugin.video.onf/icon.png plugin.video.onf/LICENCE plugin.video.onf/main.py plugin.video.onf/url_web.py
+zip -r plugin.video.onf.extended.zip plugin.video.onf/addon.xml plugin.video.onf/fanart.jpg plugin.video.onf/icon.png plugin.video.onf/LICENSE plugin.video.onf/main.py plugin.video.onf/url_web.py
 cd plugin.video.onf
 mv main.py main_extended.py
 mv main.py.bak main.py
